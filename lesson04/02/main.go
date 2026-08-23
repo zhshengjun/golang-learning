e135a5e8f46f6680a1d0dcf63956f015fcc9a4a8
zhangshengjun <stupidzhang@aliyun.com>
2026-08-23 19:53:56 +0800

    feat：移除config

--- lesson04/02/main.go
+++ lesson04/02/main.go
@@ -3,22 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"strings"
 
 	"github.com/spf13/viper"
 )
 
 func init() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("当前目录:", cwd)
 
 	viper.SetConfigFile("config.yaml")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	err = viper.BindEnv("account.from.private", "ACCOUNT_FROM_PRIVATE")
+	err := viper.BindEnv("account.from.private", "ACCOUNT_FROM_PRIVATE")
+	err = viper.BindEnv("account.from.address", "ACCOUNT_FROM_ADDRESS")
+	err = viper.BindEnv("account.to.address", "ACCOUNT_TO_ADDRESS")
+	err = viper.BindEnv("transaction.hash", "TRANSACTION_HASH")
 	if err = viper.ReadInConfig(); err != nil {
 		log.Fatalf("读取配置失败: %v", err)
 	}
