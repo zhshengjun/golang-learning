3bf9a6858d5aa58568226311590eb3cacba4a4bb
zhangshengjun <stupidzhang@aliyun.com>
2026-08-24 10:25:04 +0800

    feat：作业5

---
+++ homework/05/account.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile("config.yaml")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	err := viper.BindEnv("account.from.privateKey", "ACCOUNT_FROM_PRIVATE")
+	err = viper.BindEnv("account.to.address", "ACCOUNT_TO_ADDRESS")
+	err = viper.BindEnv("rwa.url", "RAW_URL")
+	if err != nil {
+		return
+	}
+	if err = viper.ReadInConfig(); err != nil {
+		log.Fatalf("读取配置失败: %v", err)
+	}
+}
+
+func main() {
+	//  查询余额
+	BalanceAtByPrivateKey(viper.GetString("account.from.privateKey")) //根据私钥获取
+
+	// 获取区块信息
+	BlockInfo(big.NewInt(int64(rpc.LatestBlockNumber)))
+
+	// 模拟交易
+	Transaction(viper.GetString("account.from.privateKey"), viper.GetString("account.to.address"), 0.5)
+}
+
+// Transaction 模拟交易
+func Transaction(privateHex string, toAddressHex string, amount float64) {
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancelFunc()
+	client, err := ethclient.DialContext(ctx, viper.GetString("raw.url"))
+	if err != nil {
+		log.Fatalf("failed to connect to raw client: %v", err)
+	}
+	defer client.Close()
+
+	privateKey, _ := crypto.HexToECDSA(privateHex)
+	form := Private2Address(privateHex)
+	to := common.HexToAddress(toAddressHex)
+	nonce, _ := client.PendingNonceAt(ctx, form)
+
+	header, err := client.HeaderByNumber(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to get header: %v", err)
+	}
+
+	baseFee := header.BaseFee
+	// 若不支持EIP-1559
+	if baseFee == nil {
+		suggestGasPrice, _ := client.SuggestGasPrice(ctx)
+		baseFee = suggestGasPrice
+	}
+
+	gasTipCap, _ := client.SuggestGasTipCap(ctx)
+
+	gasFeeCap := new(big.Int).Add(new(big.Int).Mul(baseFee, big.NewInt(2)), gasTipCap)
+
+	// 这里计算wei，注意amount不能直接转int，容易丢数据
+	value, _ := new(big.Float).Mul(big.NewFloat(amount), big.NewFloat(1e18)).Int(new(big.Int))
+
+	// 尝试计算
+	gasLimit, _ := client.EstimateGas(ctx, ethereum.CallMsg{
+		From:  form,
+		To:    &to,
+		Value: value,
+		Data:  nil,
+	})
+
+	maxCost := new(big.Int).Add(value, new(big.Int).Mul(gasFeeCap, big.NewInt(int64(gasLimit))))
+
+	balanceAt, _ := client.BalanceAt(ctx, form, nil)
+
+	if balanceAt.Cmp(maxCost) < 0 {
+		log.Fatalf("Balance is not enough to pay for %s", to)
+	}
+
+	chainID, _ := client.ChainID(ctx)
+	// 构造交易体
+	txData := types.DynamicFeeTx{
+		ChainID:   chainID,
+		Nonce:     nonce,
+		GasTipCap: gasTipCap,
+		GasFeeCap: gasFeeCap,
+		Gas:       gasLimit,
+		To:        &to,
+		Value:     value,
+		Data:      nil,
+	}
+
+	tx := types.NewTx(&txData)
+	// 签名 ,注意 signer需要匹配
+	transaction, err := types.SignTx(tx, types.LatestSignerForChainID(chainID), privateKey)
+	if err != nil {
+		log.Fatalf("failed to sign transaction: %v", err)
+	}
+
+	err = client.SendTransaction(ctx, transaction)
+	if err != nil {
+		log.Fatalf("failed to send transaction: %v", err)
+	}
+	fmt.Printf("transaction sent successfully")
+	fmt.Printf("Tx Hash :%s\n", transaction.Hash().Hex())
+}
+
+// BlockInfo 获取Block 的信息
+func BlockInfo(blockNumber *big.Int) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancelFunc()
+	client, err := ethclient.DialContext(ctx, viper.GetString("raw.url"))
+	if err != nil {
+		log.Fatalf("failed to connect to raw client: %v", err)
+	}
+	defer client.Close()
+
+	block, err := client.BlockByNumber(ctx, blockNumber)
+	if err != nil {
+		log.Fatalf("failed to get header by block number: %v", err)
+	}
+	fmt.Println("=================Block Info============================")
+	fmt.Printf("Block Number: %d\n", block.Number().Int64())
+	fmt.Printf("Block Hash: %s\n", block.Hash().Hex())
+	fmt.Printf("Block ParentHash: %s\n", block.ParentHash().Hex())
+	fmt.Printf("Block TxHash: %s\n", block.TxHash().Hex())
+	fmt.Printf("Block Transactions Number: %d\n", len(block.Transactions()))
+	fmt.Printf("Block GasUsed: %d(wei)\n", block.GasUsed())
+	fmt.Printf("Block Time: %s\n", time.Unix(int64(block.Time()), 0).Format(time.RFC3339))
+}
+
+// BalanceAtByPrivateKey 根据私钥获取余额
+func BalanceAtByPrivateKey(privateKey string) {
+	//1. 私钥解析
+	address := Private2Address(privateKey)
+
+	BalanceAtByAddress(address.Hex()) // 根据地址获取余额
+}
+
+func Private2Address(privateKey string) common.Address {
+	privateToECDSA, err := crypto.HexToECDSA(privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	publicKey := privateToECDSA.PublicKey
+
+	address := crypto.PubkeyToAddress(publicKey)
+
+	fmt.Printf("privateKey to address: %v\n", address.Hex())
+	return address
+}
+
+// BalanceAtByAddress 根据地址获取余额
+func BalanceAtByAddress(addressStr string) {
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancelFunc()
+	client, err := ethclient.DialContext(ctx, viper.GetString("raw.url"))
+	if err != nil {
+		log.Fatalf("failed to connect to raw client: %v", err)
+	}
+
+	address := common.HexToAddress(addressStr)
+	balance, err := client.BalanceAt(ctx, address, nil)
+	if err != nil {
+		log.Fatalf("failed to get balance of address %s: %v", addressStr, err)
+	}
+
+	fmt.Printf("BalanceAtByAddress: %d(wei)\n", balance.Int64())
+	fmt.Printf("BalanceAtByAddress: %s(ETH)\n", new(big.Rat).SetFrac(balance, big.NewInt(1e18)).FloatString(4))
+
+}
---
+++ homework/05/go.mod
@@ -0,0 +1,48 @@
+module 04
+
+go 1.26
+
+require (
+	github.com/ethereum/go-ethereum v1.17.5
+	github.com/spf13/viper v1.21.0
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/ProjectZKM/Ziren/crates/go-runtime/zkvm_runtime v0.0.0-20251001021608-1fe7b43fc4d6 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/bits-and-blooms/bitset v1.20.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/consensys/gnark-crypto v0.18.1 // indirect
+	github.com/crate-crypto/go-eth-kzg v1.5.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/c-kzg-4844/v2 v2.1.8 // indirect
+	github.com/fjl/jsonw v0.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/sagikazarmark/locafero v0.11.0 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
+	github.com/spf13/afero v1.15.0 // indirect
+	github.com/spf13/cast v1.10.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/supranational/blst v0.3.16 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/otel v1.41.0 // indirect
+	go.opentelemetry.io/otel/metric v1.41.0 // indirect
+	go.opentelemetry.io/otel/trace v1.41.0 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+)
